92d41064cebb67636764cf634fcdc7539199f7da
lampra1 <31213906+lampra1@users.noreply.github.com>
2020-07-12 03:21:20 +0300

    Update for python3 and openwrt

--- pvoutput.sh
+++ pvoutput.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 while true; do
-	python ./canadian_reads.py
-	echo "python script erro, sleeping few seconds and call it again"
+	python3 ./canadian_reads.py
+	echo "python3 script error, sleeping few seconds and call it again"
 	sleep 60s
 done
